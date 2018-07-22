623f7cc8d8151bd072c6ba8fb2d5d4e28748da71
jordan lys <jordan.lys@7shifts.com>
2018-07-21 20:23:33 -0600

    set up the server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const fs = require('fs');
+
+const dbfile = 'chatdb';
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -8,15 +11,39 @@ app.get('/', function(req, res){
 
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+    const messages = readDatabaseMessages();
+    messages.forEach(message => io.emit('init', message.msg));
+
+    socket.on('chat message', function(msg) {
+        writeDatabase(socket.client.id, msg, new Date());
+        io.emit('chat message', msg);
+    });
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
+const readDatabase = () => {
+    return JSON.parse(fs.readFileSync(dbfile, 'utf8'));
+}
+
+const readDatabaseMessages = () => {
+    const messagesObj = readDatabase();
+    return messagesObj.messages;
+}
+
+const writeDatabase = (id, msg, date) => {
+    const databaseList = readDatabase();
+	const newMessage = {
+		date : date,
+        msg : msg,
+        id: id
+    };
+    databaseList.messages.push(newMessage);
+	const json = JSON.stringify(databaseList, null, 4);
+	fs.writeFile(dbfile, json, 'utf8', (err) => {});
+}
 
 /*
 
