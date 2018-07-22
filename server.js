460d750de48e76b0785d925f5c0ce2b64bcfda62
Tyler <5952757+TylerHann@users.noreply.github.com>
2018-07-21 19:01:46 -0600

    Write/read database

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+const dbfile = 'chatdb';
+
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello World\n');
+	writeDatabase();
+	res.end(readDatabase());
 });
 
 server.listen(port, hostname, () => {
 	console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+function readDatabase() {
+	fs.readFile(dbfile, 'utf8', (err, data) => {
+		if(err) { console.log(err) }
+		else {
+			const obj = JSON.parse(data); //now it an object
+			debugger;
+			return JSON.stringify(obj);
+		}
+	});
+}
+
+function writeDatabase() {
+	const obj = {
+		date : new Date(),
+		msg : "Hello Jordan"
+	};
+
+	const json = JSON.stringify(obj);
+
+	fs.writeFile(dbfile, json, 'utf8', (err) => {});
+}
